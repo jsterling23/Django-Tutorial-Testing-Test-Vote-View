c88466acb91b380e7aca679dbfdbb9730e343e2f
Jerry Padilla <jerrypadilla23@gmail.com>
2018-07-26 21:03:31 -0500

    edits

--- test_views.py
+++ test_views.py
@@ -21,9 +21,11 @@ def create_choice(question, choice_text):
 
 class QuestionVoteViewTests(TestCase):
     """
-    This test will: vote on a choice attached to a question created -> by url 'polls:vote'.
+    - This test will: vote on a choice attached to a question created -> by url 'polls:vote'.
     Then redirect to 'polls:results' displaying the question, choice and vote that has been 
     incrimented.
+    - The key part here is the 'data={'choice': 1}' which acts as the req.POST data that the
+    vote view requires in order to pass the Try Except conditional.
     """
 
     def test_vote_view_with_voting(self):
@@ -47,8 +49,10 @@ class QuestionVoteViewTests(TestCase):
             fetch_redirect_response=True)
 
     """
-    This test will: Not vote on a choice attached to a question created --> by url 'polls:vote'. 
+    - This test will: Not vote on a choice attached to a question created --> by url 'polls:vote'. 
     Fail, then direct back to 'detail.html' with error message "You forgot to select anything you idiot".
+    - The results.html page will populate the error message "you forgot to select anything you idiot" if 
+    the Except conditon is triggered. Condition triggered cause there is no "vote" or req.POST data passed in.
     """
 
     def test_vote_view_without_voting(self):
